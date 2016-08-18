a2f3925f521d0737f14833c2b55cde5b246257c7
ArturoOntiveros <arturo.o.s@outlook.com>
2016-08-17 20:52:26 -0500

    test a alumno

--- test/models/alumno_test.rb
+++ test/models/alumno_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class AlumnoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+def setup
+	@ptr=alumnos(:one)
+end
+
+test "student not null"
+	assert_not_nil(@user,'student null')
+end
+
+test "student not empty"
+	assert_not_empty(@ptr.nombre, 'student withput name')
+end
+
 end
